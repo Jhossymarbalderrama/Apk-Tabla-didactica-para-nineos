7b524e038ac84f857e8c7c07c184bef85d11b8ef
unknown <jhossymarbalderrama@gmail.com>
2022-04-20 10:35:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Juego para niños',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
